6369cfdfb443d788a49895b35cf4c55f4dca8352
Greatkn1ght <greatkn1gh1@gmail.com>
2025-07-03 15:34:35 +0600

    HW: Did hw7

--- HW/hw.py
+++ HW/hw.py
@@ -3,6 +3,76 @@ import sqlite3
 connect = sqlite3.connect("Clothes.db")
 cursor = connect.cursor()
 
+
+def create_table():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users(
+            userid INTEGER PRIMARY KEY AUTOINCREMENT,
+            fio VARCHAR (100) NOT NULL,
+            age INTEGER NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS grades(
+            gradeid INTEGER PRIMARY KEY AUTOINCREMENT,
+            userid INTEGER, 
+            subject VARCHAR (100) NOT NULL,
+            grade INTEGER NOT NULL,
+            FOREIGN KEY (userid) REFERENCES users(userid)
+        )
+    ''')
+    connect.commit()
+
+
+create_table()
+
+def add_user(fio, age):
+    cursor.execute('INSERT INTO users(fio, age) VALUES (?, ?)', (fio, age))
+    connect.commit()
+    print(f"User {fio} is added!")
+
+
+add_user("John Doe", 25)
+add_user("Peter Parker", 19)
+add_user("Ivan Zolo", 23)
+
+def add_grade(userid, subject, grade):
+    cursor.execute(
+        'INSERT INTO grades(userid, subject, grade) VALUES (?, ?, ?)',
+        (userid, subject, grade)
+    )
+    print(f"Grade is added for user with ID {userid}")
+    connect.commit()
+
+
+def get_users_with_grades():
+    cursor.execute('''
+        SELECT users.fio, grades.subject, grades.grade
+        FROM users INNER JOIN grades ON users.userid = grades.userid
+    ''')
+    rows = cursor.fetchall()
+    for i in rows:
+        print(f"FIO: {i[0]}, SUBJECT: {i[1]}, GRADE: {i[2]}")
+
+def create_view_Daniel():
+    cursor.execute('''
+            CREATE VIEW IF NOT EXISTS elder_users AS
+            SELECT fio, age
+            FROM users
+            WHERE age >= 20
+        ''')
+    connect.commit()
+    print("View elder_users is created!")
+
+
+create_view_Daniel()
+
+def get_from_view_Daniel():
+    cursor.execute('SELECT * FROM elder_users')
+    print(cursor.fetchall())
+
+"""
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Clothes(
         brand VARHCAR (100) NOT NULL,
@@ -47,6 +117,7 @@ def delete_cloth(rowid):
     connect.commit()
 
 delete_cloth(5)
+"""
 """
 nums = list(map(int, input().split()))
 target = int(input())
